c39c5595403cab642e50924e45573f24411686b9
alexxrichard <alexandre.richard@calvados.fr>
2018-02-15 17:17:08 +0100

    suite

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
-// prod.js - production keys here!!
 module.exports = {
-    googleClientId:process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI:process.env.GOOGLE_CLIENT_MONGO_URI
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY
 };
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const express = require("express");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const mongoose = require("mongoose");
+const keys = require('./config/key');
+
+
 require('./services/passport');
+
 
 mongoose.connect(keys.mongoURI);
 
+
 const app = express();
+
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey],
+    }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes')(app);
 
-const PORT  = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log('Server start'); });
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Serveur démarré sur le port : ' + PORT);
+});
