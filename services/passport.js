989cd14651fe9d1a492e03fe1db37187f2539994
alexxrichard <alexandre.richard@calvados.fr>
2018-02-15 15:39:26 +0100

    mise a jour 20180215

--- services/passport.js
+++ services/passport.js
@@ -2,12 +2,14 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const keys = require('../config/keys');
 
+const User = require('../models/User');
+
 passport.use(
     new GoogleStrategy({
         clientID:keys.googleClientId,
         clientSecret:keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
-        proxy:true
+        /*proxy:true*/
     }, function(accessToken,refreshToken, profile, done) {
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
             return done(err, user);
